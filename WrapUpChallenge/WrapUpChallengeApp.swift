56837ad3a653b03c3d046220b9a94cc247067bf9
Lukas Herman <4lift4@gmail.com>
2022-04-05 16:27:11 +0200

    Initial Commit

---
+++ WrapUpChallenge/WrapUpChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrapUpChallengeApp.swift
+//  WrapUpChallenge
+//
+//  Created by Lukas Herman on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct WrapUpChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
